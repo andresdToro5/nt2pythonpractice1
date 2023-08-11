ba8c2335637f6f211f6d1618ebb2afabb6907966
SEBAS <js.sierramunerajuansebastian999@gmail.com>
2023-08-11 11:11:32 -0500

    nt2 se ingresa nueva variable en proyecto

--- codigoUno.py
+++ codigoUno.py
@@ -1,4 +1,9 @@
 nombreUsuario = input("Ingresar el nombre")
 direccionCasa = input("Ingresar la direccion")
 edad = input("Digitar la edad")
+
+#Forma de convertir los datos que ingresan por el metodo input en datos numericos en una sola linea de codigo.
 edad = int(input("digite su edad"))
+cantidadHijos = int(input("Digitar el numero de hijos"))
+
+
